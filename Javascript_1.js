cf33dc3903e9914b41c5e26198e6dec5633c83dc
lhwjenny <82764848+lhwjenny@users.noreply.github.com>
2022-05-07 02:25:02 +0900

    Web_Programming

---
+++ Javascript_1.js
@@ -0,0 +1,8 @@
+        //int add(int a, int b){return a+b;}
+function over(obj) {
+    obj.src='https://dummyimage.com/200x200/000/fff.jpg&text=dummy+image';
+}
+function out(obj) {
+    obj.src = 'media/couple.jpg';
+}
+
